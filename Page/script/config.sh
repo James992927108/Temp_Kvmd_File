c7deb66dca34865cc52af31a020c98a39622312a
Anthony <Anthony_teng@ausu.com>
2021-02-02 20:19:11 +0800

    fix pikvm-repo error

--- Page/script/config.sh
+++ Page/script/config.sh
@@ -10,24 +10,24 @@ export TIMEZONE='Asia/Taipei'
 export REPO_URL='http://mirror.archlinuxarm.org'
 
 # export BUILDER_URL='https://github.com/mdevaev/pi-builder'
-# export PIKVM_REPO_URL = https://pikvm.org/repos
-# export PIKVM_REPO_KEY = 912C773ABBD1B584
+# export PIKVM_REPO_URL='https://pikvm.org/repos'
+export PIKVM_REPO_KEY='912C773ABBD1B584'
 
 export BUILDER_URL='https://github.com/Yura80/pi-builder'
 export PIKVM_REPO_URL='https://raw.githubusercontent.com/Yura80/pikvm-repo/main'
-export PIKVM_REPO_KEY='39CCCE14BFBB5D69'
+# export PIKVM_REPO_KEY='39CCCE14BFBB5D69'
 
 export _BUILDER_DIR='./pi-builder'
 # export NEW_HTTPS_CERT=
 
-echo "Go to init.sh.."
-. ./init.sh
+# echo "Go to init.sh.."
+# . ./init.sh
 
-echo "Go to os.sh.."
-. ./os.sh
+# echo "Go to os.sh.."
+# . ./os.sh
 
-echo "Go to pikvm-repo.sh.."
-. ./pikvm-repo.sh
+# echo "Go to pikvm-repo.sh.."
+# . ./pikvm-repo.sh
 
 echo "Go to pikvm.sh.."
 . ./pikvm.sh
--- Page/script/pikvm-repo.sh
+++ Page/script/pikvm-repo.sh
@@ -1,14 +1,16 @@
-pacman-key --keyserver hkps://hkps.pool.sks-keyservers.net -r $PIKVM_REPO_KEY \
-		|| pacman-key --keyserver keyserver.ubuntu.com -r $PIKVM_REPO_KEY 
+pacman-key --keyserver hkps://hkps.pool.sks-keyservers.net -r $PIKVM_REPO_KEY
+
+pacman-key --keyserver keyserver.ubuntu.com -r $PIKVM_REPO_KEY 
 
 pacman-key --lsign-key $PIKVM_REPO_KEY && echo -e "\n[pikvm]" >> /etc/pacman.conf 
-
 
 case "$ARCH" in \
         arm) \
             case "$BOARD" in \
                 zero|zerow) echo "Server = https://pikvm.org/repos/zerow" >> /etc/pacman.conf;; \
                 rpi|rpi2|rpi3|rpi4) echo "Server = https://pikvm.org/repos/$BOARD-$ARCH" >> /etc/pacman.conf;; \
+                generic) echo "Server = https://raw.githubusercontent.com/Yura80/pikvm-repo/main/$BOARD-$ARCH" >> /etc/pacman.conf;; \
+                *) echo "Not support"; exit ;; \
             esac;; \
         aarch64) \
             case "$BOARD" in \
