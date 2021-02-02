a39d5b87793307f55cc34b53eeddbfb7ea43d8db
Anthony_Teng <Anthony_Teng@asus.com>
2021-02-02 18:03:22 +0800

    update config.sh
    Change-Id: I6182ca6da789278558b490e0bf37de0d7429c9fd

--- Page/script/config.sh
+++ Page/script/config.sh
@@ -2,8 +2,8 @@
 
 echo "Setup the environment variable.."
 export BOARD='generic'
-export ARCH='aarch64'
-# export ARCH='arm'
+# export ARCH='aarch64'
+export ARCH='arm'
 export PLATFORM='v2-hdmiusb'
 export LOCALE='en_US'
 export TIMEZONE='Asia/Taipei'
@@ -13,15 +13,15 @@ export REPO_URL='http://mirror.archlinuxarm.org'
 # export PIKVM_REPO_URL = https://pikvm.org/repos
 # export PIKVM_REPO_KEY = 912C773ABBD1B584
 
-export BUILDER_URL = 'https://github.com/Yura80/pi-builder'
+export BUILDER_URL='https://github.com/Yura80/pi-builder'
 export PIKVM_REPO_URL='https://raw.githubusercontent.com/Yura80/pikvm-repo/main'
 export PIKVM_REPO_KEY='39CCCE14BFBB5D69'
 
 export _BUILDER_DIR='./pi-builder'
 # export NEW_HTTPS_CERT=
 
-# echo "Go to init.sh.."
-# source ./init.sh
+echo "Go to init.sh.."
+. ./init.sh
 
 echo "Go to os.sh.."
 . ./os.sh
